40e33660c6fb8b87e4c45ad441497fecc9990002
Bruno Wolff <bruno.wolff@clinicorp.com>
2023-08-26 12:59:54 -0300

    refactor(app.ts): remove unused imports and commented code for better code readability feat(app.ts): add error handling for missing payload in dynamoDB event feat(app.ts): parse dynamoDB payload and extract deviceCode and rekognitionPayload feat(app.ts): implement countLabel function to count instances of a specific label in rekognitionPayload feat(app.ts): create digestedObject with deviceCode and passengerCount properties feat(app.ts): update main function to return digestedObject

--- app.ts
+++ app.ts
@@ -1,57 +1,37 @@
-import { S3Event } from "aws-lambda";
-import { PrismaClient } from '@prisma/client';
-import { SNS } from "@aws-sdk/client-sns";
-const prisma = new PrismaClient()
-
-type DetectLabelsParams = {
-  bucketName: string;
-  objectName: string;
-};
-/* 
-async function detectLabels({ bucketName, objectName }: DetectLabelsParams) {
-  const desiredLabels = ["Person", "Knife", "Gun", "Weapon"];
-
-  return new Rekognition().detectLabels({
-    Image: {
-      S3Object: {
-        Bucket: bucketName,
-        Name: objectName,
-      },
-    },
-    MinConfidence: parseInt(process.env.REKOGNITION_MIN_CONFIDENCE || "80"),
-    Settings: {
-      GeneralLabels: {
-        LabelInclusionFilters: desiredLabels,
-      },
-    },
-  });
-}
-
-async function insertDynamoDBItem(data: Record<string, any>) {
-  const dynamoDB = new DynamoDB({ region: "us-east-1" });
-
-  return dynamoDB.putItem({
-    TableName: "lvb-analysis-payload",
-    Item: {
-      id: { S: nanoid() },
-      payload: { S: JSON.stringify(data) },
-      insertedAt: { S: new Date().toISOString() }
-    },
-  });
-}
-
 function countLabel(labels: string[], rekognitionPayload: any) {
   return rekognitionPayload.Labels.filter((label: { Name: string }) => labels.includes(label.Name)).reduce((acc, label) => acc + label.Instances.length, 0);
-} */
+}
 
-const main = async (event: S3Event) => {
-  return new SNS({
-    region: "us-east-1",
-  })
-    .publish({
-      TopicArn: "arn:aws:sns:us-east-1:033809494047:teste",
-      Message: JSON.stringify(event),
-    });
+const main = async (events: EventPayload[]) => {
+  const [event] = events;
+  const data = JSON.parse(Buffer.from(event.data, 'base64').toString('utf-8'));
+  const dynamoPayload = data?.dynamodb?.NewImage?.payload?.S;
+
+  if (!dynamoPayload) throw new Error('No payload found');
+
+  const { deviceCode, rekognitionPayload } = JSON.parse(dynamoPayload);
+
+  const digestedObject = {
+    deviceCode,
+    passengerCount: countLabel(['Person'], rekognitionPayload),
+  }
+
+  return digestedObject;
 };
+
+interface EventPayload {
+  eventSource: string;
+  eventVersion: '1.0';
+  eventID: string;
+  eventName: string;
+  invokeIdentityArn: string;
+  awsRegion: string;
+  eventSourceARN: string;
+  kinesisSchemaVersion: string;
+  partitionKey: string;
+  sequenceNumber: string;
+  data: string;
+  approximateArrivalTimestamp: number;
+}
 
 export { main };
