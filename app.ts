b0c31dab9f55ec8003556f4086ce01a1aeaeba59
Bruno Wolff <bruno.wolff@clinicorp.com>
2023-09-03 01:21:40 -0300

    refactor(app.ts): improve type safety and readability in countLabel function feat(app.ts): add support for detecting and counting multiple labels in rekognitionPayload feat(app.ts): add type definitions for Label, Instance, BoundingBox, Parent, Alias, Category, and LabelData to improve code clarity and maintainability

--- app.ts
+++ app.ts
@@ -1,27 +1,27 @@
-function countLabel(labels: string[], rekognitionPayload: any) {
+function countLabel(labels: string[], rekognitionPayload: LabelData) {
   return rekognitionPayload.Labels.filter((label: { Name: string }) => labels.includes(label.Name)).reduce((acc: number, label: any) => acc + label.Instances.length || 1, 0);
 }
 
 const main = async (events: EventPayload[]) => {
-  console.log("chegou um evento aqui!", events);
   const [event] = events;
   const data = JSON.parse(Buffer.from(event.data, 'base64').toString('utf-8'));
   const dynamoPayload = data?.dynamodb?.NewImage?.payload?.S;
 
   if (!dynamoPayload) throw new Error('No payload found');
 
-  const { deviceCode, rekognitionPayload, timestamp } = JSON.parse(dynamoPayload);
+  const { deviceCode, rekognitionPayload, timestamp } = JSON.parse(dynamoPayload) as { deviceCode: string; rekognitionPayload: LabelData; timestamp: number };
 
   const digestedObject = {
     deviceCode,
     passengerCount: countLabel(['Person'], rekognitionPayload),
+    detectedLabels: rekognitionPayload.Labels.map(label => ({ name: label.Name, count: countLabel([label.Name], rekognitionPayload) })),
     timestamp: timestamp,
   }
 
   return digestedObject;
 };
 
-interface EventPayload {
+type EventPayload = {
   eventSource: string;
   eventVersion: '1.0';
   eventID: string;
@@ -35,5 +35,43 @@ interface EventPayload {
   data: string;
   approximateArrivalTimestamp: number;
 }
+
+type Label = {
+  Name: string;
+  Confidence: number;
+  Instances: Instance[];
+  Parents: Parent[];
+  Aliases: Alias[];
+  Categories: Category[];
+};
+
+type Instance = {
+  BoundingBox: BoundingBox;
+  Confidence: number;
+};
+
+type BoundingBox = {
+  Width: number;
+  Height: number;
+  Left: number;
+  Top: number;
+};
+
+type Parent = {
+  Name: string;
+};
+
+type Alias = {
+  Name: string;
+};
+
+type Category = {
+  Name: string;
+};
+
+type LabelData = {
+  Labels: Label[];
+  LabelModelVersion: string;
+};
 
 export { main };
